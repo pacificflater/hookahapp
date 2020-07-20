33ed7295d0b8b933b64dd8083b2ff43a34e06e2f
chingizdiarov <chingizdiarov@gmail.com>
2020-07-20 19:40:16 +0300

    Добавлена уникальность по полям

--- hookahapp/sklad/serializers.py
+++ hookahapp/sklad/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from rest_framework.validators import UniqueTogetherValidator
 from hookahapp.sklad.models import Flavour, Manufacturer, Membership, Mix
 
 
@@ -13,10 +13,21 @@ class ManufacturerSerializer(serializers.ModelSerializer):
 
 
 
+
 class FlavourCreateSerializer(serializers.ModelSerializer):
     class Meta:
+
         model = Flavour
         fields = ['id', 'flavour_name', 'manufacturer', 'in_stock', 'add_time']
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset = Flavour.objects.all(),
+                fields = ['flavour_name', 'manufacturer'],
+                message='Such flavour already exists'
+            )
+        ]
+
 
 class FlavourSerializer(FlavourCreateSerializer):
     manufacturer = ManufacturerSerializer()
@@ -28,11 +39,26 @@ class ManufacturerListSerializer(serializers.ModelSerializer):
         model = Manufacturer
         fields = ['id', 'name', 'flavours']
 
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Manufacturer.objects.all(),
+                fields=['name'],
+                message='Such manufacturer name already exists'
+            )
+        ]
+
 
 class MembershipCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Membership
         fields = ['id', 'percentage', 'mix', 'flavour']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Membership.objects.all(),
+                fields=['mix', 'flavour'],
+                message='Such Membership already exists'
+            )
+        ]
 
 
 class MembershipSerializer(MembershipCreateSerializer):
@@ -46,3 +72,10 @@ class MixSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mix
         fields = ['id', 'mix_name', 'rating', 'strength', 'compound']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Mix.objects.all(),
+                fields=['mix_name'],
+                message='Such Mix name already exists'
+            )
+        ]
