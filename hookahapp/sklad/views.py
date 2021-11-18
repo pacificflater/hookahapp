39209baa9b05d85658cd0d8f1255f5a813be9098
chingizdiarov <chingizdiarov@gmail.com>
2021-11-18 20:33:10 +0300

    Откат авторизации по API

--- hookahapp/sklad/views.py
+++ hookahapp/sklad/views.py
@@ -1,13 +1,13 @@
 from hookahapp.sklad.models import Manufacturer, Flavour, Mix, Membership, FlavourType, ManufacturerType, BowlType, Bowl
 from hookahapp.sklad.serializers import ManufacturerListSerializer, FlavourSerializer, MixSerializer, MembershipSerializer, MembershipCreateSerializer, FlavourCreateSerializer, FlavourTypeSerializer, ManufacturerTypeSerializer, ManufacturerCreateSerializer, BowlSerializer, BowlTypeSerializer, BowlCreateSerializer, MixCreateSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from django.shortcuts import render
 
 class BowlView(viewsets.ModelViewSet):
     serializer_class = BowlSerializer
     queryset = Bowl.objects.all().order_by('name')
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     def get_serializer_class(self):
         if self.action == "create":
             return BowlCreateSerializer
@@ -18,11 +18,11 @@ class BowlView(viewsets.ModelViewSet):
 class BowlTypeView(viewsets.ModelViewSet):
     serializer_class = BowlTypeSerializer
     queryset = BowlType.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
 
 class Manufacturers(viewsets.ModelViewSet):
     queryset = Manufacturer.objects.all().order_by('name')
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     filterset_fields = ['name', 'type']
     def get_serializer_class(self):
         if self.action == "create":
@@ -34,7 +34,7 @@ class Manufacturers(viewsets.ModelViewSet):
 
 class FlavoursView(viewsets.ModelViewSet):
     queryset = Flavour.objects.all().order_by('manufacturer__name', 'flavour_name')
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     filter_fields = ['flavour_name', 'manufacturer__name', 'manufacturer__type',  'flavour_type__type', 'in_stock']
 
     def get_serializer_class(self):
@@ -46,24 +46,24 @@ class FlavoursView(viewsets.ModelViewSet):
 
 class FlavourTypeView(viewsets.ModelViewSet):
     serializer_class = FlavourTypeSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     queryset = FlavourType.objects.all().order_by('type')
 
 class ManufacturerTypeView(viewsets.ModelViewSet):
     serializer_class = ManufacturerTypeSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     queryset = ManufacturerType.objects.all()
 
 
 class MixesInStockView(viewsets.ModelViewSet):
     serializer_class = MixSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     flavours_out_of_stock = Flavour.objects.filter(in_stock=False)
     queryset = Mix.objects.exclude(compound__in=flavours_out_of_stock).order_by('-rating','mix_name')
 
 
 class MixesView(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     queryset = Mix.objects.all().order_by('-rating','mix_name')
     filter_fields = ['mix_name', 'compound']
     def get_serializer_class(self):
@@ -74,7 +74,7 @@ class MixesView(viewsets.ModelViewSet):
         return MixSerializer
 
 class MembershipView(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     queryset = Membership.objects.all().order_by('flavour__flavour_name')
     def get_serializer_class(self):
         if self.action == "create":
